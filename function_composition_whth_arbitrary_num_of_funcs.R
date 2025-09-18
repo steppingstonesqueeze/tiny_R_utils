e3247aa4ab0f858439c9d5fe8c4bbd1d8d2c3cbd
steppingstonesqueeze <viennacoup@proton.me>
2025-09-18 15:21:59 +0530

    Very simple function compositions with an arbitrary number of functions with more examples anbd also including howto write pipe operators

--- function_composition_whth_arbitrary_num_of_funcs.R
+++ function_composition_whth_arbitrary_num_of_funcs.R
@@ -33,3 +33,36 @@ compose_v2 <- function(...) {
 
 f <- compose_v2(square, double, inc)
 f(3)
+
+# pipe operators in the magrittr / dplyr style - left to right func applications 
+
+pipe_v1 <- function(...) {
+  funcs <- list(...)
+  function(x) {
+    Reduce(function(acc, f) f(acc), funcs, init = x)
+  }
+}
+
+g <- pipe_v1(inc, double, square)  # ((x + 1) * 2)^2
+g(3)  # 64
+
+# finally -- checking that all arguments passed are actually functions so that the composition is safe
+
+compose_safe <- function(...) {
+  funcs <- list(...)
+  stopifnot(all(vapply(funcs, is.function, logical(1))))
+  function(x) Reduce(function(acc, f) f(acc), rev(funcs), init = x)
+}
+
+g1 <- compose_safe(square, double, inc)
+g1(3)
+
+# unsafe
+g1 <- compose_safe(square, 44, inc)
+g1(3)
+
+
+
+
+
+
